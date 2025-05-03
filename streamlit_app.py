1e82cb54d97e255d839875a709746a11491cf521
Omneya Zayed <112699385+zayed95@users.noreply.github.com>
2025-05-03 16:20:08 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('Diabetese Prediction Site')
 
-st.write('This is an app that predicts whether you have diabetes or not by asking you a few questions about your overall mental and physical health')
+st.info('This is an app that predicts whether you have diabetes or not by asking you a few questions about your overall mental and physical health')
