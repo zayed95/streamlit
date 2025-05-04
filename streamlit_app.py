ec85db0f5583b60c84b7419840161b5622fda010
Omneya Zayed <112699385+zayed95@users.noreply.github.com>
2025-05-04 14:24:49 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,10 @@ import streamlit as st
 st.title('Diabetese Prediction Site')
 
 st.info('This is an app that predicts whether you have diabetes or not by asking you a few questions about your overall mental and physical health')
+
+with st.sidebar:
+  st.header('Input Features')
+  HighBP = st.toggle('High Blood Pressure')
+  BMI = st.slider('BMI', 0, 18.5, 24.9, 29.9, 34.9, 39.9, 100)
+  GenHlth = st.radio('Rate your general health with 1 being the best and 5 being the poorest', [1, 2, 3, 4, 5])
+  
